e578c34b7832e5d17b34536e4281fdfa6ce1f481
Alan Smith <alan@randomsmiths.com>
2018-11-29 21:47:40 -0500

    Add wireless functionality

--- Sprint2/Sprint2.ino
+++ Sprint2/Sprint2.ino
@@ -1,6 +1,15 @@
 // Include the Servo library
 #include <Servo.h>
+#include "ds_interface.h"
 
+#define RX_FROM_NMCU 2
+#define TX_TO_NMCU 3
+
+// ---------------------------------------------------------------------------
+/*
+   Create a Drive Station Interface "DS_Interface" object
+*/
+DS_Interface ds(RX_FROM_NMCU, TX_TO_NMCU);
 // Declare the variables
 
 Servo ServoGrabber;
@@ -18,58 +27,91 @@ const int PIN_SERVO_BACK_LEFT = 6;
 
 void setup()
 {
-    Serial.begin(9600); // Starts the serial communication
-    // Attach Servo Pin numbers
-    ServoGrabber.attach(PIN_SERVO_GRABBER);
-    ServoFrontRight.attach(PIN_SERVO_FRONT_RIGHT);
-    ServoFrontLeft.attach(PIN_SERVO_FRONT_LEFT);
-    ServoBackLeft.attach(PIN_SERVO_BACK_LEFT);
-    ServoBackRight.attach(PIN_SERVO_BACK_RIGHT);
+  Serial.begin(115200);
+  Serial.println("Interfacing arduino with nodemcu");
+  ds.init();     // setup drive station comms
+  // Attach Servo Pin numbers
+  ServoGrabber.attach(PIN_SERVO_GRABBER);
+  ServoFrontRight.attach(PIN_SERVO_FRONT_RIGHT);
+  ServoFrontLeft.attach(PIN_SERVO_FRONT_LEFT);
+  ServoBackLeft.attach(PIN_SERVO_BACK_LEFT);
+  ServoBackRight.attach(PIN_SERVO_BACK_RIGHT);
 
-    // Reset the wheels
-    stopWheels();
+  // Reset the wheels
+  stopWheels();
 }
 
 void loop()
 {
-    driveSpeed(100, 100);
-    delay(1000);
-    stopWheels();
-    delay(1000);
+  char input = ds.readInputIfAvailable();
+  switch(input){
+    case 't':
+       testServos();
+       break;
+    case 'w':
+       driveFullSpeed();
+       break;
+    case 'd':
+       turnRight();
+       break;
+    case 'a':
+       turnLeft();
+       break;
+    case 's':
+       stopWheels();
+       break;
+    case 'x':
+       driveBackwards();
+       break;
+    default:
+       break;
+  }
 }
 
 void testServos()
 {
-    ServoBackLeft.write(180);
-    delay(500);
-    ServoBackLeft.write(90);
-    ServoBackRight.write(0);
-    delay(500);
-    ServoBackRight.write(90);
-    ServoFrontLeft.write(180);
-    delay(500);
-    ServoFrontLeft.write(90);
+  ServoBackLeft.write(180);
+  delay(500);
+  ServoBackLeft.write(90);
+  ServoBackRight.write(0);
+  delay(500);
+  ServoBackRight.write(90);
+  ServoFrontLeft.write(180);
+  delay(500);
+  ServoFrontLeft.write(90);
 
-    ServoFrontRight.write(0);
-    delay(500);
-    stopWheels();
+  ServoFrontRight.write(0);
+  delay(500);
+  stopWheels();
 }
 
+void turnRight()
+{
+  driveSpeed(100,-100);
+}
+void turnLeft()
+{
+  driveSpeed(-100, 100);
+}
+void driveBackwards()
+{
+  driveSpeed(-100,-100);
+}
 void driveFullSpeed()
 {
-    driveSpeed(100, 100);
+  driveSpeed(100, 100);
 }
 
 void stopWheels()
 {
-    driveSpeed(0, 0);
+  driveSpeed(0, 0);
 }
 
 // This is -100 to 100 with 0 being stopped
 void driveSpeed(int leftMotorSpeed, int rightMotorSpeed)
 {
-    ServoBackLeft.write(map(leftMotorSpeed, -100, 100, 0, 180));
-    ServoBackRight.write(map(rightMotorSpeed, -100, 100, 180, 0));
-    ServoFrontLeft.write(map(leftMotorSpeed, -100, 100, 0, 180));
-    ServoFrontRight.write(map(rightMotorSpeed, -100, 100, 180, 0));
+  ServoBackLeft.write(map(leftMotorSpeed, -100, 100, 0, 180));
+  ServoBackRight.write(map(rightMotorSpeed, -100, 100, 180, 0));
+  ServoFrontLeft.write(map(leftMotorSpeed, -100, 100, 0, 180));
+  ServoFrontRight.write(map(rightMotorSpeed, -100, 100, 180, 0));
 }
